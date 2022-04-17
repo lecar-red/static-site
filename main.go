f78282e957cfdae1527b53380cdb8f6cb0e73005
Lee Carmichael <lecar_red@yahoo.com>
2022-04-17 12:28:00 -0500

    add error handling

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"html/template"
 	"log"
 	"net/http"
+	"os"
 	"path/filepath"
 )
 
@@ -26,13 +27,34 @@ func serveTemplate(w http.ResponseWriter, r *http.Request) {
 	lp := filepath.Join("templates", "layout.html")
 	fp := filepath.Join("templates", filepath.Clean(r.URL.Path))
 
-	tmpl, err := template.ParseFiles(lp, fp)
+	info, err := os.Stat(fp)
 	if err != nil {
-		//log.Fatal(err)
-		log.Println(err)
-		w.WriteHeader(http.StatusNotFound)
+		log.Println("os.Stat:", err.Error())
+		if os.IsNotExist(err) {
+			http.NotFound(w, r)
+			return
+		}
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
 		return
 	}
 
-	tmpl.ExecuteTemplate(w, "layout", nil)
+	if info.IsDir() {
+		http.NotFound(w, r)
+		return
+	}
+
+	tmpl, err := template.ParseFiles(lp, fp)
+	if err != nil {
+		log.Println("ParseFiles:", err.Error())
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
+
+	err = tmpl.ExecuteTemplate(w, "layout", nil)
+	if err != nil {
+		log.Println("ExecuteTemplate:", err.Error())
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
+
 }
