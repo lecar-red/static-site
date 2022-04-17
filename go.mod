85f90550b2c9b2ed80cf2cb59b8a1282a3a82006
Lee Carmichael <lecar_red@yahoo.com>
2022-04-17 12:20:49 -0500

    make some templates

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/lecar-red/static-site
+
+go 1.17
--- main.go
+++ main.go
@@ -1,17 +1,38 @@
 package main
 
 import (
+	"html/template"
 	"log"
 	"net/http"
+	"path/filepath"
 )
 
 func main() {
 	port := ":8181"
 	fs := http.FileServer(http.Dir("./static"))
-	http.Handle("/", fs)
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
+
+	http.HandleFunc("/", serveTemplate)
 
 	log.Println("listening on ", port)
 	if err := http.ListenAndServe(port, nil); err != nil {
 		log.Fatal(err)
 	}
+}
+
+func serveTemplate(w http.ResponseWriter, r *http.Request) {
+	log.Println("serving page:", r.URL.Path)
+
+	lp := filepath.Join("templates", "layout.html")
+	fp := filepath.Join("templates", filepath.Clean(r.URL.Path))
+
+	tmpl, err := template.ParseFiles(lp, fp)
+	if err != nil {
+		//log.Fatal(err)
+		log.Println(err)
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+
+	tmpl.ExecuteTemplate(w, "layout", nil)
 }
